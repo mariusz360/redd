fc4beaedbc868dfe25dea737fbd6469ccb51a6f3
Mariusz Lapinski <1840613+mariusz360@users.noreply.github.com>
2022-08-15 00:24:37 -0400

    Update searchable.rb.

--- lib/redd/models/searchable.rb
+++ lib/redd/models/searchable.rb
@@ -19,7 +19,7 @@ module Redd
       # @option params [:relevance, :hot, :top, :new, :comments] :sort the sort order of results
       # @option params [String] :restrict_to restrict by subreddit (prefer {Subreddit#search})
       # @return [Listing<Comment, Submission>] the search results
-      def search(query, **params)
+      def search(query, params)
         params[:q] = query
         params[:t] = params.delete(:time) if params.key?(:time)
         if params[:restrict_to]
